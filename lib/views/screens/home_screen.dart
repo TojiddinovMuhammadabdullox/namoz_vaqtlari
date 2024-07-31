351b8490b69dae3a81e0d2c69ff8600ab8535e1a
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-07-31 21:04:04 +0500

    responsive

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -193,9 +193,14 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildCurrentTimeAndPrayer() {
+    double width = MediaQuery.of(context).size.width;
+    double fontSize = width * 0.1;
+    double smallFontSize = width * 0.05;
+    double padding = width * 0.04;
+
     return Container(
-      margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.all(16),
+      margin: EdgeInsets.all(padding),
+      padding: EdgeInsets.all(padding),
       decoration: BoxDecoration(
         color: Colors.white.withOpacity(0.1),
         borderRadius: BorderRadius.circular(20),
@@ -212,7 +217,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Text(
             DateFormat('HH:mm:ss').format(_currentTime),
             style: TextStyle(
-              fontSize: 48,
+              fontSize: fontSize,
               color: Colors.white,
               fontWeight: FontWeight.bold,
               shadows: [
@@ -227,7 +232,7 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(height: 16),
           Text(
             DateFormat('d MMMM yyyy').format(_currentTime),
-            style: const TextStyle(fontSize: 18, color: Colors.white70),
+            style: TextStyle(fontSize: smallFontSize, color: Colors.white70),
           ),
           const SizedBox(height: 16),
           Row(
@@ -236,14 +241,15 @@ class _HomeScreenState extends State<HomeScreen> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
+                  Text(
                     'Hozirgi namoz',
-                    style: TextStyle(fontSize: 14, color: Colors.white70),
+                    style: TextStyle(
+                        fontSize: smallFontSize, color: Colors.white70),
                   ),
                   Text(
                     _currentPrayer,
-                    style: const TextStyle(
-                        fontSize: 24,
+                    style: TextStyle(
+                        fontSize: smallFontSize * 1.5,
                         color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
@@ -252,14 +258,15 @@ class _HomeScreenState extends State<HomeScreen> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  const Text(
+                  Text(
                     'Keyingi namoz',
-                    style: TextStyle(fontSize: 14, color: Colors.white70),
+                    style: TextStyle(
+                        fontSize: smallFontSize, color: Colors.white70),
                   ),
                   Text(
                     _nextPrayer,
-                    style: const TextStyle(
-                        fontSize: 24,
+                    style: TextStyle(
+                        fontSize: smallFontSize * 1.5,
                         color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
@@ -270,8 +277,10 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(height: 16),
           Text(
             'Qolgan vaqt: $_timeUntilNextPrayer',
-            style: const TextStyle(
-                fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: smallFontSize * 1.2,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -279,111 +288,76 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildRegionDropdown() {
+    double width = MediaQuery.of(context).size.width;
+    double fontSize = width * 0.05;
+    double padding = width * 0.04;
+
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      margin: EdgeInsets.symmetric(horizontal: padding),
+      padding: EdgeInsets.symmetric(horizontal: padding, vertical: padding / 2),
       decoration: BoxDecoration(
         color: Colors.white.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton<String>(
-          value: _selectedRegion,
-          onChanged: (String? newValue) {
-            if (newValue != null) {
-              setState(() {
-                _selectedRegion = newValue;
-                _prayerTimes = null;
-              });
-              _fetchPrayerTimes();
-            }
-          },
-          items: _regions.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value, style: const TextStyle(color: Colors.white)),
-            );
-          }).toList(),
-          dropdownColor: const Color(0xFF1F2F98),
-          icon: const Icon(Icons.arrow_drop_down, color: Colors.white),
-          isExpanded: true,
-          style: const TextStyle(fontSize: 18, color: Colors.white),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPrayerTimesList() {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (BuildContext context, int index) {
-          final prayerNames = [
-            'Bomdod',
-            'Quyosh',
-            'Peshin',
-            'Asr',
-            'Shom',
-            'Xufton'
-          ];
-          final prayerTimes = [
-            _prayerTimes!.fajr,
-            _prayerTimes!.sunrise,
-            _prayerTimes!.dhuhr,
-            _prayerTimes!.asr,
-            _prayerTimes!.maghrib,
-            _prayerTimes!.isha,
-          ];
-          final icons = [
-            Icons.nightlight_round,
-            Icons.wb_sunny,
-            Icons.wb_sunny_outlined,
-            Icons.wb_twighlight,
-            Icons.nights_stay,
-            Icons.bedtime,
-          ];
-          return _buildPrayerTimeItem(
-              prayerNames[index], prayerTimes[index], icons[index]);
-        },
-        childCount: 6,
-      ),
-    );
-  }
-
-  Widget _buildPrayerTimeItem(String name, String time, IconData icon) {
-    final isCurrentPrayer = name == _currentPrayer;
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: isCurrentPrayer
-            ? const Color(0xFF3D4EC6)
-            : Colors.white.withOpacity(0.1),
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.1),
-            blurRadius: 5,
-            spreadRadius: 1,
+            blurRadius: 10,
+            spreadRadius: 5,
           ),
         ],
       ),
-      child: ListTile(
-        leading: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.2),
-            shape: BoxShape.circle,
-          ),
-          child: Icon(icon, color: Colors.white),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: _selectedRegion,
+          onChanged: (newValue) {
+            setState(() {
+              _selectedRegion = newValue!;
+              _fetchPrayerTimes();
+            });
+          },
+          items: _regions.map((region) {
+            return DropdownMenuItem<String>(
+              value: region,
+              child: Text(
+                region,
+                style: TextStyle(fontSize: fontSize, color: Colors.white),
+              ),
+            );
+          }).toList(),
         ),
-        title: Text(
-          name,
-          style: const TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-        ),
-        trailing: Text(
-          time,
-          style: const TextStyle(color: Colors.white, fontSize: 18),
-        ),
+      ),
+    );
+  }
+
+  SliverList _buildPrayerTimesList() {
+    double width = MediaQuery.of(context).size.width;
+    double fontSize = width * 0.05;
+
+    final prayers = [
+      ('Bomdod', _prayerTimes!.fajr),
+      ('Quyosh', _prayerTimes!.sunrise),
+      ('Peshin', _prayerTimes!.dhuhr),
+      ('Asr', _prayerTimes!.asr),
+      ('Shom', _prayerTimes!.maghrib),
+      ('Xufton', _prayerTimes!.isha),
+    ];
+
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final prayer = prayers[index];
+          return ListTile(
+            title: Text(
+              prayer.$1,
+              style: TextStyle(fontSize: fontSize, color: Colors.white),
+            ),
+            trailing: Text(
+              prayer.$2,
+              style: TextStyle(fontSize: fontSize, color: Colors.white),
+            ),
+          );
+        },
+        childCount: prayers.length,
       ),
     );
   }
